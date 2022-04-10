6cb4d1f751cac3773607b6e65776bbd7a4e3fbb8
parkervoit <parker.r.voit@gmail.com>
2022-04-10 17:10:49 -0500

    edited readme and updated dice module with abilty score lookup

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Roll Initiative! 
 ### A simple dice rolling app
 
-I forgot my dice one D&D session, so I decided to code a simple dice rolling app using streamlit and python
+I forgot my dice one D&D session, so I decided to code a simple dice rolling app using Streamlit.io and Python. 
+
+
+### Dependencies
+Required dependencies:
+- Git
+- Streamlit
+
+### Notes
+Read the notebook for an example of how to use the tool.
--- dice.py
+++ dice.py
@@ -1,43 +1,48 @@
-import random 
+import random
+import json
 
-def dice_roll(x = 20, y = 0, modifier = 0, numdice = 1, damage = False):
+with open('roll-initiative/char_sheet.json') as file:
+    char_sheet = json.load(file)
+
+def dice_roll(x = 20, addl_dice = 0, ability = '', numdice = 1, crit = False):
     ''' dice_roll(x, modifier, numdice)
         
         x = int n sides of the dice (default d20)
-        y = int n sides of added die, granted from a spell like Guidance (default 0)
+        addl_dice = int n sides of added die, granted from a spell like Guidance (default 0)
         modifier = int value of modifier for roll (default 0)
         numdice = int n number of dice (default 1)
+        crit = whether or not the hit roll was a critical hit (default False)
+        
 
         This function returns the final total for a roll in D&D
         Prints each dice roll, base sum, modifier, and the total roll value
+        This function handles critical hits by duplicating the damage die, then adding the ability modifier.
+        
         
         Returns dice, additional_dice, modifier
     '''
     count = 1
     dice = 0
     add_dice = 0
+    output = ''
+    modifier = char_sheet['abilities'][ability]['mod']
     for i in range(numdice):
         count += 1
         roll = random.randint(1,x)
-        print(f'<|>  d{x} #{count - 1} Base      :  {roll}  <|>')
-        if y != 0:
-            add_dice += random.randint(1,y)
-            print(f'<|>  Bonus Dice Roll  :  {add_dice}  <|>')
-        if damage == True:
-            if roll == 20:
-                dice += (roll * 2)
-            else:
-                dice += roll
-        else:
-            dice += roll
-    output = f'\n\n Base Total       :  {dice + add_dice}\n Modifier         :  {modifier:{"+" if modifier else ""}}\n-----------------------------\n Total Roll       :  {dice + modifier + add_dice}'
+        output += f'<|>  d{x} #{count - 1} Dice Roll :  {roll}  <|>\n'
+        dice += roll
+    if crit == True:
+        dice *= 2
+    if addl_dice != 0:
+            add_dice += random.randint(1,addl_dice)
+            output += f'<|>  d{addl_dice} Bonus Roll   :  {add_dice}  <|>\n'
+    output += f'''\n\n Dice Total          :  {dice + add_dice}\n Modifier            :  {modifier:{"+" if modifier else ""}}\n-----------------------------\n Total Roll          :  {dice + modifier + add_dice}'''
     return dice, add_dice, modifier, output
 
-def roll_d20(addl_dice = 0, modifier = 0):
+def roll_d20(addl_dice = 0, ability = ''):
     ''' addl_dice = int n sides of bonus dice
         modifier = int ability modifier
 
         returns base sum of dice, bonus dice, modifier, and an output 
             string with total. '''
-    return dice_roll(20, addl_dice, modifier, 1, False)
-    
+    return dice_roll(20, addl_dice, ability, 1, False)
